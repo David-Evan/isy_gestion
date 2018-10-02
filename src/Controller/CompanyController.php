1b92f2a8736b8691b33097ed2fdb424afc95ab4e
David-Evan <devan.exa@gmail.com>
2018-10-02 15:08:22 +0200

    add product management

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -2,36 +2,92 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 use App\Entity\Product;
+use App\Form\ProductType;
 
 class CompanyController extends AbstractController
 {
     /**
      * @Route("/company", name="company")
      */
-    public function index()
-    {
+    public function index(){
         return $this->render('company/index.html.twig', [
             'controller_name' => 'CompanyController',
         ]);
     }
 
     /**
-     * @Route("/company/products", name="company_products_list")
+     * @Route("/company/products", name="company_product_list", methods={"GET","HEAD"})
      */
-    public function products(){
+    public function listProduct(){
         $products = $this->getDoctrine()
-                        ->getRepository(Product::class)
-                        ->findAll();
+                          ->getRepository(Product::class)
+                          ->findAll();
 
-        if (!$products) {
-        throw $this->createNotFoundException('No product found');
+        return $this->render('company/product-list.html.twig', [
+            'Products' => $products,
+        ]);
+    }
+
+    /**
+     * @Route("/company/products/add", name="company_product_add")
+     */
+    public function addProduct(Request $request){
+
+        $product = new Product();
+        $form =  $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+    
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre produit a été ajouté !'
+            );
+
+            return $this->redirectToRoute('company_product_list');
         }
 
-        return $this->render('company/products-list.html.twig', [
-            'Products' => $products,
+        return $this->render('company/product-add.html.twig', [
+            'AddProductForm' => $form->createView(),  
+        ]);
+    }
+
+    /**
+     * @Route("/company/products/update/{id}", name="company_product_update", requirements={"id"="\d+"})
+     */
+    public function updateProduct(Request $request, Product $product){
+        
+        $form =  $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+    
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre produit a été modifé !'
+            );
+
+            return $this->redirectToRoute('company_product_list');
+        }
+
+        return $this->render('company/product-update.html.twig', [
+            'UpdateProductForm' => $form->createView(),  
         ]);
     }
 }
---
+++ src/Form/ProductType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\{TextType, SubmitType, NumberType, MoneyType}; 
+
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('code')
+            ->add('description', TextType::class, ['required' => false])
+            ->add('tax')
+            ->add('units')
+            ->add('price')
+            ->add('maxDiscount', NumberType::class, ['required' => false])
+            ->add('Enregistrer', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
